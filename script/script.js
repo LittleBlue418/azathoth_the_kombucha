3b5558f6171209bb8a87ed06f19e4fc90bb8b728
LittleBlue418 <hollyathomas88@gmail.com>
2019-12-08 21:34:49 +0100

    initial commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+$(() => {
+
+  load_story(initial)
+
+})
+
+function load_story(state) {
+  // Find the div
+  // Find the corresponding JSON file
+  // Get the correct object by ID
+  // Input that text into the text div
+  // Input the image address into
+
+}
