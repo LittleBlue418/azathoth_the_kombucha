363255f6b87c0276abc9c20b6dbb2dae10df6578
LittleBlue418 <hollyathomas88@gmail.com>
2019-12-09 15:16:44 +0100

    moving functions into the class

--- script/script.js
+++ script/script.js
@@ -1,68 +1,71 @@
 $(() => {
 
-class Story {
-  constructor() {
-    this.current_chapter = 1
-    this.chapter_title = $("#story_chapter")
-    this.chapter_text = $("#story_text")
-    this.chapter_image = $("#story_image")
-  }
+  class Story {
+    constructor() {
+      this.current_chapter = 1
+      this.max_chapters = 0
+      this.chapter = {}
+      this.book = {}
 
+      this.chapter_title = $("#story_chapter")
+      this.chapter_text = $("#story_text")
+      this.chapter_image = $("#story_image")
 
-  next_chapter() {
-    if (this.current_chapter < max_chapters) {
-      this.current_chapter += 1;
-      load_chapter(this.current_chapter)
-    } else {
-      console.log("last chapter!")
+      this.load_chapter = this.load_chapter.bind(this)
+    }
+
+    load_book() {
+      $.get("assets/story.json", (json) => {
+        this.book = json
+        this.max_chapters = Object.keys(this.book).length
+      })
+      .then(this.load_chapter)
+      .then(post_chapter)
+    }
+
+    load_chapter() {
+      console.log(this)
+      this.chapter = this.book[`${this.current_chapter}`]
+    }
+
+    next_chapter() {
+      if (this.current_chapter < this.max_chapters) {
+        this.current_chapter += 1;
+        load_chapter(this.current_chapter)
+      } else {
+        console.log("last chapter!")
+      }
+    }
+
+    previous_chapter() {
+      if (this.current_chapter > 1) {
+        this.current_chapter -= 1;
+        load_chapter(this.current_chapter)
+      } else {
+        console.log("chapter 1!")
+      }
     }
   }
 
-  previous_chapter() {
-    if (this.current_chapter > 1) {
-      this.current_chapter -= 1;
-      load_chapter(this.current_chapter)
-    } else {
-      console.log("chapter 1!")
-    }
-  }
-}
-
-var story = new Story;
+  var story = new Story;
 
 
-// Global variables
-var max_chapters;
-var chapter = "x";
-var book = "y";
+  function post_chapter() {
 
-
-
-function load_chapter() {
-
-    story.chapter_title.text(chapter["title"])
-    story.chapter_text.text(chapter["text"])
-    story.chapter_image.text(chapter["image"])
-    console.log(chapter)
-    console.log(book)
+    story.chapter_title.text(story.chapter["title"])
+    story.chapter_text.text(story.chapter["text"])
+    story.chapter_image.text(story.chapter["image"])
+    console.log(story.chapter)
+    console.log(story.book)
   }
 
 
-function get_chapter(current_chapter) {
-  $.get("assets/story.json", (json) => {
-    book = json
-    chapter = json[`${current_chapter}`]
-    console.log(chapter)
-    max_chapters = Object.keys(book).length
-    return chapter
-  })
-    .then(load_chapter())
-}
+  story.load_book(story.current_chapter)
+  console.log(story.chapter)
+  console.log(story.book)
 
+  //.then(post_chapter)
 
-
-
-  get_chapter(3)
 })
 
 
