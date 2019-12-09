40b2368d816c018bdf78bcbf5eb0a37762ca101b
LittleBlue418 <hollyathomas88@gmail.com>
2019-12-09 10:35:54 +0100

    building a story class

--- script/script.js
+++ script/script.js
@@ -1,14 +1,68 @@
 $(() => {
 
-  load_story(initial)
+class Story {
+  constructor() {
+    this.current_chapter = 1
+    this.chapter_title = $("#story_chapter")
+    this.chapter_text = $("#story_text")
+    this.chapter_image = $("#story_image")
+  }
 
+
+  next_chapter() {
+    if (this.current_chapter < max_chapters) {
+      this.current_chapter += 1;
+      load_chapter(this.current_chapter)
+    } else {
+      console.log("last chapter!")
+    }
+  }
+
+  previous_chapter() {
+    if (this.current_chapter > 1) {
+      this.current_chapter -= 1;
+      load_chapter(this.current_chapter)
+    } else {
+      console.log("chapter 1!")
+    }
+  }
+}
+
+var story = new Story;
+
+
+// Global variables
+var max_chapters;
+var chapter = "x";
+var book = "y";
+
+
+
+function load_chapter() {
+
+    story.chapter_title.text(chapter["title"])
+    story.chapter_text.text(chapter["text"])
+    story.chapter_image.text(chapter["image"])
+    console.log(chapter)
+    console.log(book)
+  }
+
+
+function get_chapter(current_chapter) {
+  $.get("assets/story.json", (json) => {
+    book = json
+    chapter = json[`${current_chapter}`]
+    console.log(chapter)
+    max_chapters = Object.keys(book).length
+    return chapter
+  })
+    .then(load_chapter())
+}
+
+
+
+
+  get_chapter(3)
 })
 
-function load_story(state) {
-  // Find the div
-  // Find the corresponding JSON file
-  // Get the correct object by ID
-  // Input that text into the text div
-  // Input the image address into
 
-}
